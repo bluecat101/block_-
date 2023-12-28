69e7c97db388302696dbacd87c25895101ecfd0a
bluecat101 <97800922+bluecat101@users.noreply.github.com>
2023-12-28 18:06:04 +0900

    can make any childlen for duble crossover

--- crossover.js
+++ crossover.js
@@ -13,20 +13,37 @@ function single_crossover(child,parent){
   }
   
   function double_crossover(child,parent){
-    var rand1 = Math.floor(Math.random()*chrom);
-    var rand2 = rand1 + Math.floor(Math.random()*(chrom-rand1));//0<=rand1<rand2<1000
-    // console.log(rand1,rand2);
-    //二点交叉
+    // var child_len = child.length;
+    var rand_len = Math.log(NUM_OF_CHILD) / Math.log(2)-1;
+    // console.log(NUM_OF_CHILD,rand_len);
+    var rand = new Array(rand_len);
+    for(var i=0;i<rand_len;i++){
+      tmp = Math.floor(Math.random()*chrom);
+      while(true){
+        if(!rand.includes(tmp)){
+          rand[i] = tmp;
+          break;
+        }
+      }
+    }
+    rand.sort();
+    var rand_position =0;
+    var choiced_parent =0;
+    var count=0;
     for(var c=0;c<chrom;c++){
-        if(c<rand1){
-            child[0][c] = parent[0][c];
-            child[1][c] = parent[1][c];
-        }else if(rand1<=c && c<rand2){
-            child[0][c] = parent[1][c];
-            child[1][c] = parent[0][c];
-        }else{
-            child[0][c] = parent[0][c];
-            child[1][c] = parent[1][c];
-        };
-    };
+      if(c>=rand[rand_position]){
+        rand_position++;
+      }
+      for(var i=0;i<NUM_OF_CHILD;i++){
+        // console.log(choiced_parent);
+        // console.log(child[i],parent[choiced_parent][c])
+        child[i][c] = parent[choiced_parent][c];
+        count++;
+        if(count>=2**(rand_len-1-rand_position)){
+          count=0;
+          choiced_parent = choiced_parent ^ 1;
+        }
+      }
+    }
+    // child;
   }
--- selection.js
+++ selection.js
@@ -1,15 +1,15 @@
 function selection(child,individual,sorted_eval){
   //--- 淘汰  
-  var min = new Array(2);
-  for(var i=0;i<2;i++){
+  var min = new Array(NUM_OF_CHILD);
+  for(var i=0;i<NUM_OF_CHILD;i++){
     min[i] = new Array(2);
+    min[i][0] = sorted_eval[sorted_eval.length-(i+1)][0]
+    min[i][1] = sorted_eval[sorted_eval.length-(i+1)][1]
   };
-  min[0][0] = sorted_eval[sorted_eval.length-1][0]
-  min[0][1] = sorted_eval[sorted_eval.length-1][1]
-  min[1][0] = sorted_eval[sorted_eval.length-2][0]
-  min[1][1] = sorted_eval[sorted_eval.length-2][1]
+  
   for(var c=0;c<chrom;c++){
-    individual[min[0][1]][c] = child[0][c]; 
-    individual[min[1][1]][c] = child[1][c];
+    for(var i=0;i<NUM_OF_CHILD;i++){
+      individual[min[i][1]][c] = child[i][c]; 
+    }
   };
 }
