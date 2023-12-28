2e5dcae30f497f86a037a638cbcc997101868da1
bluecat101 <97800922+bluecat101@users.noreply.github.com>
2023-12-28 18:39:00 +0900

    can make any childlen for single crossover

--- crossover.js
+++ crossover.js
@@ -1,51 +1,55 @@
 function single_crossover(child,parent){
+  var rand = getRandoms(NUM_OF_CHILD/2);
   //--- 単一交叉
-    var rand1 = Math.floor(Math.random()*chrom);
-    for(var c=0;c<chrom;c++){
-        if(c<rand1){
-            child[0][c] = parent[0][c];
-            child[1][c] = parent[1][c];
-        }else{
-            child[0][c] = parent[1][c];
-            child[1][c] = parent[0][c];
-        }
+  // var rand1 = Math.floor(Math.random()*chrom);
+  for(var c=0;c<chrom;c++){
+    for(var i=0;i<NUM_OF_CHILD/2;i++){
+      if(c<rand[i]){
+        child[i][c] = parent[0][c];
+        child[NUM_OF_CHILD-1-i][c] = parent[1][c];
+      }else{
+        child[i][c] = parent[1][c];
+        child[NUM_OF_CHILD-1-i][c] = parent[0][c];
+      }  
     }
   }
+}
   
-  function double_crossover(child,parent){
-    // var child_len = child.length;
-    var rand_len = Math.log(NUM_OF_CHILD) / Math.log(2)-1;
-    // console.log(NUM_OF_CHILD,rand_len);
-    var rand = new Array(rand_len);
-    for(var i=0;i<rand_len;i++){
-      tmp = Math.floor(Math.random()*chrom);
-      while(true){
-        if(!rand.includes(tmp)){
-          rand[i] = tmp;
-          break;
-        }
+function double_crossover(child,parent){
+  var rand_len = Math.log(NUM_OF_CHILD) / Math.log(2)-1;
+  var rand = getRandoms(rand_len);
+  var rand_position =0;
+  var choiced_parent =0;
+  var count=0;
+  for(var c=0;c<chrom;c++){
+    if(c>=rand[rand_position]){
+      rand_position++;
+    }
+    for(var i=0;i<NUM_OF_CHILD;i++){
+      // console.log(choiced_parent);
+      // console.log(child[i],parent[choiced_parent][c])
+      child[i][c] = parent[choiced_parent][c];
+      count++;
+      if(count>=2**(rand_len-1-rand_position)){
+        count=0;
+        choiced_parent = choiced_parent ^ 1;
       }
     }
-    rand.sort();
-    var rand_position =0;
-    var choiced_parent =0;
-    var count=0;
-    for(var c=0;c<chrom;c++){
-      if(c>=rand[rand_position]){
-        rand_position++;
-      }
-      for(var i=0;i<NUM_OF_CHILD;i++){
-        // console.log(choiced_parent);
-        // console.log(child[i],parent[choiced_parent][c])
-        child[i][c] = parent[choiced_parent][c];
-        count++;
-        if(count>=2**(rand_len-1-rand_position)){
-          count=0;
-          choiced_parent = choiced_parent ^ 1;
-        }
-      }
-    }
-    child.shift();
-    child.pop();
-    // child;
   }
+  child.shift();
+  child.pop();
+}
+function getRandoms(length){
+  var rand = new Array(length);
+  for(var i=0;i<length;i++){
+    tmp = Math.floor(Math.random()*chrom);
+    while(true){
+      if(!rand.includes(tmp)){
+        rand[i] = tmp;
+        break;
+      }
+    }
+  }
+  return rand.sort(); 
+}
+
--- selection.js
+++ selection.js
@@ -1,14 +1,15 @@
 function selection(child,individual,sorted_eval){
+  // 淘汰  
+  // double_crossoverの際に親と同じコピーが存在し、削除しているため個数がNUM_OF_CHILDと異なる。
   var child_len = child.length;
-  console.log(child_len); // double_crossoverの際に親と同じコピーが存在し、削除しているため個数がNUM_OF_CHILDと異なる。
-  //--- 淘汰  
+  // 削除する子を探す
   var min = new Array(child_len);
   for(var i=0;i<child_len;i++){
     min[i] = new Array(2);
     min[i][0] = sorted_eval[sorted_eval.length-(i+1)][0]
     min[i][1] = sorted_eval[sorted_eval.length-(i+1)][1]
   };
-  
+  // 淘汰
   for(var c=0;c<chrom;c++){
     for(var i=0;i<child_len;i++){
       individual[min[i][1]][c] = child[i][c]; 
